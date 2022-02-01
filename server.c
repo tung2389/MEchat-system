53120e7e8bd9d7e6e297faed177dfeb7756d34d3
Tung Luu <luukhanhtung2002@gmail.com>
2022-02-01 15:10:15 -0500

    Add skeleton code for server

--- server.c
+++ server.c
@@ -1,10 +1,32 @@
-// server.c
+#include "socket_util.h"
 
-#include <stdio.h>
-
-int main()
+int main(int argc, char **argv)
 {
+    char *port;
+    struct sockaddr_storage client_addr;
+    socklen_t sin_size;
+    char client_info[INET6_ADDRSTRLEN + MAX_PORT_LEN + 1];
 
+    if(argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+    port = argv[1];
 
-  return 0;
+    int listenfd = open_listenfd(port);
+    if(listenfd == -1) {
+        exit(1);
+    }
+
+    int newfd = accept(listenfd, (struct sockaddr *) &client_addr, &sin_size);
+    if(newfd == -1) {
+        fprintf(stderr, "Server failed to accept a connection");
+        exit(1);
+    }
+
+    get_sock_str((struct sockaddr *) &client_addr, client_info);
+    printf("server: got connection from %s\n", client_info);
+
+    close(newfd);
+    return 0;
 }
