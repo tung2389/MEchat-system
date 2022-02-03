76a37cc933e182694b2e10e2cf600c275d68b260
Tung Luu <luukhanhtung2002@gmail.com>
2022-02-03 16:25:21 -0500

    add sending nickname to client

--- client.c
+++ client.c
@@ -3,6 +3,11 @@
 int main(int argc, char **argv)
 {   
     char *host, *port;
+    char nickname[NICKNAME_LEN + 1]; // + 1 to account for the newline character.
+    char msg[MSG_LEN];
+    // Don't buffer on stdout, since this is a chat app.
+    setbuf(stdout, NULL);
+    
     if (argc != 3) {
         fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
         exit(1);
@@ -13,6 +18,20 @@ int main(int argc, char **argv)
     if(clientfd == -1) {
         exit(1);
     }
-  
+
+    strcpy(msg, INVALID_NICKNAME_MSG);
+    while(strcmp(msg, INVALID_NICKNAME_MSG) == 0) {
+        printf("Enter your nickname:\n");
+        fgets(nickname, NICKNAME_LEN + 1, stdin);
+        // Delete the newline character
+        nickname[strlen(nickname) - 1] = '\0';
+        send(clientfd, nickname, NICKNAME_LEN, 0);
+        recv(clientfd, msg, sizeof msg, 0);
+        if(strcmp(msg, INVALID_NICKNAME_MSG) == 0) {
+            printf("Your nickname is invalid\n");
+        }
+    }
+
+    close(clientfd);
     return 0;
 }
